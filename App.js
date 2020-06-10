c6d93186af5914e63ae5b7f0fffa64ca9781820d
yeojisun <duwltjs5921@gmail.com>
2020-06-10 15:41:51 +0900

    fetch까지

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Loading from "./Loader";
+import { Alert } from "react-native";
+import * as Location from "expo-location";
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+//import { StyleSheet, Text, View } from 'react-native';
+
+const API_KEY = "321246c0795e8a10b679a0fdf64909db";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    ); // 변수열을 문자에 포함시킬때 `(벡틱)을 쓴다
+    console.log(data);
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      //console.log(response);
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("can't find you.", "so sad");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Loader.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>getting the weather</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 20, // →←패딩값
+    paddingVertical: 100, // ↑↓패딩값
+    backgroundColor: "#CEF6CE",
+  },
+  text: {
+    color: "#2c2c2c",
+    fontSize: 30,
+  },
+});
